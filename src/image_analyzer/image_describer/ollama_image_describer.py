e5d52efb5273db70cc2fc863296f88e36fa964fe
mgmg <jeonghoson@postech.ac.kr>
2024-12-17 17:25:35 +0900

    feat: change ollama prompt

--- src/image_analyzer/image_describer/ollama_image_describer.py
+++ src/image_analyzer/image_describer/ollama_image_describer.py
@@ -1,6 +1,7 @@
 import base64
 import io
 import logging
+from collections import defaultdict
 from typing import Any
 
 import aiohttp
@@ -8,7 +9,7 @@ from PIL import Image
 from aiohttp import ClientResponse
 
 from image_analyzer.image_describer.image_describer import ImageDescriber
-from image_analyzer.object_detector.object_detector import ImageObjectDetected
+from image_analyzer.object_detector.object_detector import Detection, ImageObjectDetected
 
 __all__ = ["base64encode", "OllamaImageDescriber"]
 
@@ -16,9 +17,19 @@ logger = logging.getLogger(__name__)
 
 ollama_port: int = 11434
 ollama_addr: str = f"http://localhost:{ollama_port}/api/generate"
-ollama_max_w_h: int = 672
-ollama_model: str = "llava:7b"
-ollama_prompt: str = "Assume you are in the photo. Briefly descibe the nearby surroundings."
+ollama_max_w_h: int = 224
+ollama_model: str = "knoopx/mobile-vlm:3b-fp16"
+
+
+def get_ollama_prompt(detections: list[Detection]) -> str:
+    detections_dict: defaultdict[str, int] = defaultdict(int)
+    for detection in detections:
+        detections_dict[detection.class_name] += 1
+
+    detections_str: str = ", ".join(detections_dict.keys())
+    ollama_prompt: str = f"Briefly describe the image focusing on {detections_str}."
+    logger.info(f"Using OLLAMA prompt: {ollama_prompt}")
+    return ollama_prompt
 
 
 def base64encode(image: Image.Image) -> str:
@@ -32,16 +43,17 @@ def base64encode(image: Image.Image) -> str:
     return base64.b64encode(buffer.getvalue()).decode()
 
 
-async def query(image: Image.Image) -> str:
+async def query(image: Image.Image, detections: list[Detection]) -> str:
     """
     Query the OLLAMA server with an image.
     :param image: The image to query.
+    :param detections: The list of object detections in the image.
     :return: The response from the OLLAMA server.
     """
     async with aiohttp.ClientSession() as session:
         response: ClientResponse = await session.post(ollama_addr, json={
             "model": ollama_model,
-            "prompt": ollama_prompt,
+            "prompt": get_ollama_prompt(detections),
             "stream": False,
             "images": [base64encode(image)],
         })
@@ -60,4 +72,4 @@ class OllamaImageDescriber(ImageDescriber):
 
     async def describe_image(self, image: ImageObjectDetected) -> str:
         image_resized: Image.Image = self.preprocess(image.image)
-        return await query(image_resized)
+        return await query(image_resized, image.detections)
